68b36b3d30cce34c1479b1bb18b8c3e431ebd190
Gourab Modak <82747495+gauravthedeveloper@users.noreply.github.com>
2023-04-24 16:06:33 +0530

    Delete p.js

--- p.js
+++
@@ -1,6 +0,0 @@
-fetch("example.json")
-  .then((response) => response.text())
-  .then((text) => {
-    document.getElementById("content").textContent = text;
-  })
-  .catch((error) => console.log(error));
